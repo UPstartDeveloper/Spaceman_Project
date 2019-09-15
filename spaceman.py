6f9d6b73e3b4f1026d868c720535cef54d57847e
= <zainr7989@gmail.com>
2019-09-14 17:17:35 -0700

    Add docstrings.

--- spaceman.py
+++ spaceman.py
@@ -11,8 +11,12 @@ https://manytools.org/hacker-tools/convert-images-to-ascii-art/go
 image = Image.open('astronaut.png')
 
 
-# A function that creates a list of words from words.txt.
 def generate_words_list():
+    """
+    A function that creates a list of words from words.txt.
+    Input: words.txt file
+    Output: list of words @words_list
+    """
     f = open('words.txt', 'r')
     words_list = f.readlines()
     f.close()
@@ -20,46 +24,74 @@ def generate_words_list():
     return words_list
 
 
-# A function that chooses the word user needs to guess.
 def load_word(words_list):
+    """
+    A function that chooses the word user needs to guess.
+    Input: @param words_list a list of str objects
+    Output: str object @secret_word
+    """
     secret_word = random.choice(words_list)
     return secret_word
 
 
-# A function to switch the secret word for another (Sinister Spaceman).
 def switch_secret_word(secret_word):
-    new_secret = ""  # return value
-    susbstitute_words = list()  # captures words of equal length to secret_word
+    """
+     A function to switch the secret word for another (Sinister Spaceman).
+     Input: @param secret_word a string the user has to guess.
+     Output: another str object @new_secret the user must guess instead.
+     Check words.txt for all words of same length as secret_word, then traverse
+     that list for words with same letters,
+     and in the same indices as guessed by user so far.
+     """
+    substitute_words = list()  # captures words of equal length to secret_word
     all_words = generate_words_list()
-    guess_so_far = get_guessed_word(secret_word, letters_guessed)  # represents letters user has correct so far
+
+    # stores letters user has correct so far in secret_word
+    guess_so_far = get_guessed_word(secret_word, letters_guessed)
     for word in all_words:
         if len(word) == len(secret_word):
-            susbstitute_words.append(word)  # add all words of same length for consideration
-        '''
-        try-except error handling
-        '''
-        try:
-            for i in range(len(word)):
-                if not guess_so_far[i] == "_" or not guess_so_far[i] == " " and not word[i] == guess_so_far[i]:
-                    susbstitute_words.pop(i)  # remove words which don't have same letters as those guessed correctly
-        except IndexError:
-            for for i in range(min(0, len(word))):
-                if not guess_so_far[i] == "_" or not guess_so_far[i] == " " and not word[i] == guess_so_far[i]:
-                    susbstitute_words.pop(i)  # remove words which don't have same letters as those guessed correctly
-    new_secret = random.choice(susbstitute_words)
+            susbstitute_words.append(word)  # add all words of same length
+
+    # get rid of spaces and underscores from get_guessed_word
+    check_this_str = ""
+    for letter in guess_so_far:
+        if letter.isalpha():
+            check_this_str += letter
+
+    # delete words without same letters
+    for word in substitute_words:
+        for i in range(len(check_this_str)):
+            if check_this_str[i].isalpha() and check_this_str[i] not in word:
+                substitute_words.remove(word)
+    new_secret = random.choice(substitute_words)
+
     return new_secret
 
 
-# Checks if all the letters of the secret word have been guessed.
 def is_word_guessed(secret_word, letters_guessed):
+    """
+     A function to check if all letters of the secret word have been guessed.
+     Input: @param str object secret_word
+            @param list object letters_guessed.
+     Output: True if all letters of secret_word appear in letters_guessed,
+     False otherwise.
+     """
     for i in range(len(secret_word)):
         if not secret_word[i] in letters_guessed:
             return False
     return True
 
 
-# Returns a string after each guess to represent user's progress
 def get_guessed_word(secret_word, letters_guessed):
+    """
+    A function to return a str after each guess to represent user's progress.
+    Input: @param str secret_word
+           @param list letters_guessed
+    Output: str object guess_so_far
+    Looks for all letters in secret_word that appear in letters_guessed.
+    Matches are shown.
+    Letters in secret_word not found are represented by "_ ".
+    """
     guess_so_far = ""
     for letter in secret_word:
         if letter in letters_guessed:
@@ -69,44 +101,75 @@ def get_guessed_word(secret_word, letters_guessed):
     return guess_so_far
 
 
-# A function to check if the guessed letter is in the secret word.
 def is_guess_in_word(guess, secret_word):
+    """
+    A function to check if the guessed letter is in the secret word.
+    Input: @param str object guess
+           @param str object secret_word
+    Output: True or False.
+    Checks to see if single letter entered by user appears in secret_word.
+    """
     return (guess in secret_word)
 
 
-# A function to display letters of the alphabet not yet guessed.
 def display_alpha(alphabet, guessed_letters):
+    """
+    A function to display letters of the alphabet not yet guessed.
+    Input: @param list object alphabet
+           @param list object guessed_letters
+    Output: None.
+    alphabet stores all 26 letters at start of the game.
+    """
+
+    # Remove letters from alphabet which appear in guessed_letters
     if not len(guessed_letters) == 0:
         for i in range(len(guessed_letters)):
             if guessed_letters[i] in alphabet:
                 remove_letter = guessed_letters[i]
                 alphabet.remove(remove_letter)
 
+    # Display contents of alphabet to user.
     for letter in alphabet:
         print(letter, end="")
     print("")
 
 
-# A function that prints a dashed line to divide the output into sections.
 def divide():
+    """
+    A function that prints a dashed line to divide the output into sections.
+    Input: none.
+    Output: none.
+    """
     print("-------------------------------------")
 
 
-# A function that builds a list of incorrect guesses.
 def add_to_incorrect(letter, list_of_letters):
+    """
+    A function that builds a list of incorrect guesses.
+    Input: @param str object letter, @param list object list_of_letters
+    Output: none.
+    """
     list_of_letters.append(letter)
 
 
-# A function that displays a list of incorrect guesses.
 def display_incorrect(list_of_incorrect):
+    """
+    A function that displays a list of incorrect guesses.
+    Input: @param list object list_of_incorrect
+    Output: none.
+    """
     print("Here are the letters you guessed incorrectly:", end=" ")
     for letter in list_of_incorrect:
         print(letter, end="")
     print("")  # moves cursor to next line for formatting output
 
 
-# A function that controls the game of spaceman.
 def spaceman(secret_word):
+    """
+    A function that controls the game of spaceman.
+    Input: @param str object secret_word.
+    Output: none.
+    """
     guesses_left = len(secret_word)
     incorrect_guessed_letters = list()
     print("Welcome to Spaceman! \n" +
@@ -141,6 +204,7 @@ def spaceman(secret_word):
             print(f"You have {guesses_left} incorrect guesses left.")
             print("These letters haven't been guessed yet: ", end="")
             display_alpha(alpha, letters_guessed)
+            print("here")
             secret_word = switch_secret_word(secret_word)  # switch the secret word
             print("You have a new word to guess: {}".format(secret_word))  # *used for debugging switch_secret_word*
             divide()
@@ -169,7 +233,6 @@ def spaceman(secret_word):
             display_alpha(alpha, letters_guessed)
 
 
-# These function calls will start the game
 '''
 Creating list of alphabetic letters.
 Credit to https://stackoverflow.com/questions/16060899/alphabet-range-on-python
@@ -178,6 +241,7 @@ secret_word = load_word(generate_words_list())
 alpha = list(string.ascii_lowercase)
 letters_guessed = list()
 
+# Loops through game until user ready to quit.
 control = "Yes"
 while control == "Yes" or control == "yes":
     spaceman(secret_word)
